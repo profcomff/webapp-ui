e906fc3ed15af375f4e36eada38b25b8404421c5
Artem Netsvetaev <physphile@vk.com>
2023-03-20 12:06:48 +0300

    - move app version to services page

--- src/views/apps/AppsView.vue
+++ src/views/apps/AppsView.vue
@@ -1,6 +1,9 @@
 <script setup lang="ts">
 import { IrdomLayout } from '@/components';
 import AsyncContent from './AsyncContent.vue';
+import { computed } from 'vue';
+
+const version = computed(() => import.meta.env.VITE_APP_VERSION);
 </script>
 
 <template>
@@ -10,5 +13,13 @@ import AsyncContent from './AsyncContent.vue';
 
 			<template #fallback> Загрузка... </template>
 		</Suspense>
+		<span v-if="version" class="version">Версия приложения: {{ version }}</span>
 	</IrdomLayout>
 </template>
+
+<style scoped>
+.version {
+	opacity: 0.3;
+	font-size: 12px;
+}
+</style>
