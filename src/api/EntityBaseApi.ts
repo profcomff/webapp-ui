0b936a76efeb406c185c667f8bb7f772ed3b4e03
Artem Netsvetaev <physphile@vk.com>
2023-04-10 13:46:38 +0300

    - entity base api, - upgrade deps

---
+++ src/api/EntityBaseApi.ts
@@ -0,0 +1,47 @@
+import { BaseApi } from './BaseApi';
+import { ArrayResponse, Entity } from './models';
+
+export interface GetAllParams {
+	limit?: number;
+	offset?: number;
+}
+
+export class EntityBaseApi<
+	E extends Entity,
+	GetAllP extends GetAllParams = never,
+	ModifyB = Omit<E, 'id'>,
+	GetP = never,
+	DeleteBulkP = never,
+> extends BaseApi {
+	constructor(path: string) {
+		super(path);
+	}
+
+	public async getById(id: number, params?: GetP) {
+		return this.get<E, GetP>(`/${id}`, params);
+	}
+
+	public async getAll(params?: GetAllP) {
+		return this.get<ArrayResponse<E>, GetAllP>('/', params);
+	}
+
+	public async deleteById(id: number) {
+		return this.delete<string>(`/${id}`);
+	}
+
+	public async deleteBulk(params?: DeleteBulkP) {
+		this.delete<string, DeleteBulkP>('/bulk', params);
+	}
+
+	public async patchById(id: number, body: ModifyB) {
+		return this.patch<E, ModifyB>(`/${id}`, body);
+	}
+
+	public async create(body: ModifyB) {
+		return this.post<E, ModifyB>('', body);
+	}
+
+	public async createBulk(body: ModifyB[]) {
+		this.post<E[], ModifyB[]>('/bulk', body);
+	}
+}
