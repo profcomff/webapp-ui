b645609db7afdf32ba0f6e3996e33fdedecc88e0
Artem Netsvetaev <physphile@vk.com>
2023-04-27 14:38:43 +0300

    - copy token to clipboard #82

--- src/views/admin/AdminView.vue
+++ src/views/admin/AdminView.vue
@@ -1,14 +1,24 @@
 <script setup lang="ts">
 import { AccessAllowed, IrdomLayout } from '@/components';
+import { MaterialIcon } from '@/components/lib';
 import { scopename } from '@/models';
 import { useProfileStore } from '@/store';
 
 const profileStore = useProfileStore();
+
+const copyToken = () => {
+	if (profileStore.token) {
+		navigator.clipboard.writeText(profileStore.token).catch(err => {
+			console.error(err);
+		});
+	}
+};
 </script>
 
 <template>
 	<IrdomLayout title="Админка">
 		<p class="id">Ваш id: {{ profileStore.id }}</p>
+		<button type="button" @click="copyToken">Скопировать токен <MaterialIcon name="content_copy" /></button>
 		<RouterLink to="/admin/groups" class="link">Группы пользователей</RouterLink>
 
 		<AccessAllowed :scope="scopename.auth.scope.read" no-fallback>
