7bcfa82646f8add0be057371bdaac2dd9536e2c7
Artem Morozov <morozoff234@yandex.ru>
2023-05-15 01:02:45 +0300

    Fixes.

--- src/components/IrdomChip.vue
+++ src/components/IrdomChip.vue
@@ -25,10 +25,12 @@ withDefaults(
 
 	&:not(:disabled) {
 		&:hover {
+			background: oklch(91.59% 0.012 268.97);
 			background: color-mix(in oklab, var(--color-primary-dark) 5%, var(--background));
 		}
 
 		&:active {
+			background: oklch(88.87% 0.019 286.01);
 			background: color-mix(in oklab, var(--color-primary-dark) 9%, var(--background));
 		}
 	}
@@ -39,20 +41,26 @@ withDefaults(
 }
 
 .outlined {
+	border: 1px solid oklch(65.03% 0.004 286.29);
 	border: 1px solid color-mix(in oklab, transparent 50%, var(--color-primary-dark));
 
 	&:disabled {
+		border: 1px solid oklch(64.8% 0.006 286.23);
 		border: 1px solid color-mix(in oklab, transparent 20%, var(--color-primary-light));
 	}
 }
 
 .elevated {
 	&:not(:disabled) {
-		--background: color-mix(in oklab, transparent 83.5%, var(--color-primary));
+		--background: oklch(90.87% 0.016 278.49);
+
+		/*  --background: color-mix(in oklab, transparent 83.5%, var(--color-primary)); */
 
 		box-shadow: 1px 2px oklab(0% 0 0 / 30%), 1px 3px 1px oklch(0% 0 0 / 15%);
 	}
 
-	--background: color-mix(in oklab, transparent 83.5%, var(--color-primary-dark));
+	--background: oklch(83.52% 0.025 285.85);
+
+	/* --background: color-mix(in oklab, transparent 83.5%, var(--color-primary-dark)); */
 }
 </style>
