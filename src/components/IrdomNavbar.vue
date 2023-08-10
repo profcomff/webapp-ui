1ddf396eebc59c0e0eb0fe1a299d80d168af1fd4
Roman Dyakov <ROMAN@DYAKOV.SPACE>
2023-08-10 20:31:31 +0300

    Init element

--- src/components/IrdomNavbar.vue
+++ src/components/IrdomNavbar.vue
@@ -1,17 +1,33 @@
 <script setup lang="ts">
+import { ref } from 'vue';
+
 export interface NavbarItem {
 	name: string;
 	icon: string;
 	path: string[];
 }
 
-defineProps<{
+const props = defineProps<{
 	items: NavbarItem[];
 }>();
+
+function findFirstIndexOfStringByPrefix(arr: NavbarItem[], prefix: string) {
+	for (let i = 0; i < arr.length; i++) {
+		for (let j = 0; j < arr[i].path.length; j++) {
+			if (prefix.startsWith(arr[i].path[j])) {
+				console.debug(`Init bottom menu element is ${i}`);
+				return i;
+			}
+		}
+	}
+	return undefined;
+}
+
+const value = ref(findFirstIndexOfStringByPrefix(props.items, window.location.pathname));
 </script>
 
 <template>
-	<v-bottom-navigation grow elevation="4" mandatory>
+	<v-bottom-navigation grow elevation="4" v-model="value" mandatory>
 		<v-btn v-for="{ icon, name, path } in items" :key="name" @click="$router.push(path[0])">
 			<v-icon :icon="`md:${icon}`" />
 			{{ name }}
