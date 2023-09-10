b9bb49dd08c9525e880c36ad83c39629638c7136
Roman Dyakov <ROMAN@DYAKOV.SPACE>
2023-09-10 21:44:31 +0300

    Markdown component

---
+++ src/components/MarkdownRenderer.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+import Markdown from 'markdown-it';
+
+const props = withDefaults(defineProps<{ text: string }>(), { text: '' });
+const markdown = new Markdown();
+</script>
+
+<template>
+	<!-- eslint-disable vue/no-v-html -->
+	<div v-html="markdown.render(props.text)" />
+	<!-- eslint-enable -->
+</template>
