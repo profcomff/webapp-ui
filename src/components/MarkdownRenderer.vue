e68e1303059e615fe72c24d5929d77248b6b2d62
Dyakov Roman <ROMAN@DYAKOV.SPACE>
2023-09-11 16:40:14 +0300

    Style fix

--- src/components/MarkdownRenderer.vue
+++ src/components/MarkdownRenderer.vue
@@ -20,38 +20,44 @@ h1 {
 	margin-top: 0.67em;
 	margin-bottom: 0.47em;
 }
+
 h2 {
 	margin-top: 0.83em;
 	margin-bottom: 0.63em;
 }
+
 h3 {
 	margin-top: 1em;
 	margin-bottom: 0.8em;
 }
+
 h4 {
 	margin-top: 1.33em;
 	margin-bottom: 1.13em;
 }
+
 h5 {
 	margin-top: 1.67em;
 	margin-bottom: 1.47em;
 }
+
 h6 {
 	margin-top: 2.33em;
 	margin-bottom: 2.13em;
 }
+
 ul ul,
-ol ul {
-	margin-left: 15px;
-}
+ol ul,
 ol ol,
 ul ol {
 	margin-left: 15px;
 }
+
 a {
 	color: #1a0dab;
 	text-decoration: none;
 }
+
 a:hover {
 	text-decoration: underline;
 }
