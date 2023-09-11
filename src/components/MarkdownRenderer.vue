3f15b2ca1ebecc9a75f3fb02a7bdf7fa1b0a8253
Dyakov Roman <ROMAN@DYAKOV.SPACE>
2023-09-11 16:47:14 +0300

    Deep styles

--- src/components/MarkdownRenderer.vue
+++ src/components/MarkdownRenderer.vue
@@ -11,54 +11,54 @@ const markdown = new Markdown({
 
 <template>
 	<!-- eslint-disable vue/no-v-html -->
-	<div v-html="markdown.render(props.text)" />
+	<div class="md" v-html="markdown.render(props.text)" />
 	<!-- eslint-enable -->
 </template>
 
 <style scoped>
-h1 {
+.md >>> h1 {
 	margin-top: 0.67em;
 	margin-bottom: 0.47em;
 }
 
-h2 {
+.md >>> h2 {
 	margin-top: 0.83em;
 	margin-bottom: 0.63em;
 }
 
-h3 {
+.md >>> h3 {
 	margin-top: 1em;
 	margin-bottom: 0.8em;
 }
 
-h4 {
+.md >>> h4 {
 	margin-top: 1.33em;
 	margin-bottom: 1.13em;
 }
 
-h5 {
+.md >>> h5 {
 	margin-top: 1.67em;
 	margin-bottom: 1.47em;
 }
 
-h6 {
+.md >>> h6 {
 	margin-top: 2.33em;
 	margin-bottom: 2.13em;
 }
 
-ul ul,
-ol ul,
-ol ol,
-ul ol {
+.md >>> ul ul,
+.md >>> ol ul,
+.md >>> ol ol,
+.md >>> ul ol {
 	margin-left: 15px;
 }
 
-a {
+.md >>> a {
 	color: #1a0dab;
 	text-decoration: none;
 }
 
-a:hover {
+.md >>> a:hover {
 	text-decoration: underline;
 }
 </style>
