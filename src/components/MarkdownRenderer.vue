4380abe2d61078f0830ddea3ebeb990484ecdbf3
Dyakov Roman <ROMAN@DYAKOV.SPACE>
2023-09-11 16:32:08 +0300

    Markdown better style

--- src/components/MarkdownRenderer.vue
+++ src/components/MarkdownRenderer.vue
@@ -2,7 +2,11 @@
 import Markdown from 'markdown-it';
 
 const props = withDefaults(defineProps<{ text: string }>(), { text: '' });
-const markdown = new Markdown();
+const markdown = new Markdown({
+	breaks: true,
+	linkify: true,
+	quotes: '«»„“',
+});
 </script>
 
 <template>
@@ -10,3 +14,36 @@ const markdown = new Markdown();
 	<div v-html="markdown.render(props.text)" />
 	<!-- eslint-enable -->
 </template>
+
+<style scoped>
+h1 {
+	margin-top: 0.67em;
+	margin-bottom: 0.47em;
+}
+h2 {
+	margin-top: 0.83em;
+	margin-bottom: 0.63em;
+}
+h3 {
+	margin-top: 1em;
+	margin-bottom: 0.8em;
+}
+h4 {
+	margin-top: 1.33em;
+	margin-bottom: 1.13em;
+}
+h5 {
+	margin-top: 1.67em;
+	margin-bottom: 1.47em;
+}
+h6 {
+	margin-top: 2.33em;
+	margin-bottom: 2.13em;
+}
+ul ul, ol ul {
+	margin-left: 15px;
+}
+ol ol, ul ol {
+	margin-left: 15px;
+}
+</style>
