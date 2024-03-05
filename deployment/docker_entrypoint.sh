8d9eda93ec3796f6f2f71b3577cc883e1c100b4b
Artem <55246906+physphile@users.noreply.github.com>
2024-03-05 03:30:47 +0300

    Fix (#199)
    ## Изменения
    <!-- Опишите здесь на языке, понятном каждому, изменения, сделанные в
    исходном коде по пунктам. -->
    
    ## Детали реализации
    <!-- Здесь можно описать технические детали по пунктам. -->
    
    ## Check-List
    <!-- После сохранения у следующих полей появятся галочки, которые нужно
    проставить мышкой -->
    - [ ] Вы проверили свой код перед отправкой запроса?
    - [ ] Вы написали тесты к реализованным функциям?
    - [ ] Вы не забыли применить форматирование `black` и `isort` для
    _Back-End_ или `Prettier` для _Front-End_?
    
    ---------
    
    Co-authored-by: Artem Netsvetaev <physphile@vk.com>

--- deployment/docker_entrypoint.sh
+++ deployment/docker_entrypoint.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
-
 export APPJS_NAME=$(cd /usr/share/nginx/html && ls js/app.*.js)
-if [ ! -z ${APPJS_NAME} ]
+if [ ! -z "${APPJS_NAME}" ]
 then
-    echo 'app.js is ${APPJS_NAME}'
-    sed -i 's|APPJS_NAME|'${APPJS_NAME}'|g' /etc/nginx/conf.d/default.conf
+    echo "app.js is ${APPJS_NAME}"
+    sed -i "s|APPJS_NAME|'${APPJS_NAME}'|g" /etc/nginx/conf.d/default.conf
     sed -i 's|# ||g' /etc/nginx/conf.d/default.conf
 else
     echo 'app.js is app.js, no changes'
