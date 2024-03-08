d0eba1836dfe2fc2d9be5ff7f7aab6bb5c44870b
Artem Netsvetaev <physphile@vk.com>
2024-03-08 23:06:59 +0300

    fix touch

--- src/components/IrdomLayout.vue
+++ src/components/IrdomLayout.vue
@@ -5,15 +5,21 @@ defineProps<{
 </script>
 
 <template>
-	<v-main class="main">
-		<slot />
-	</v-main>
+	<div v-touch="touch" class="touch">
+		<v-main class="main">
+			<slot />
+		</v-main>
+	</div>
 </template>
 
 <style scoped>
+.touch,
 .main {
 	width: 100%;
 	height: 100%;
+}
+
+.main {
 	max-width: 900px;
 	padding: calc(var(--v-layout-top) + 12px) min(5%, 8px) calc(var(--v-layout-bottom) + 12px);
 	margin: 0 auto;
