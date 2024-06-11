4fe6aa8f241d91c810463365bcdb70f187692033
Roman Dyakov <ROMAN@DYAKOV.SPACE>
2024-06-11 12:12:15 +0300

    No ssl on dev

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,6 @@ import { fileURLToPath, URL } from 'url';
 import { VitePWA } from 'vite-plugin-pwa';
 import Vue from '@vitejs/plugin-vue';
 import postcssPresetEnv from 'postcss-preset-env';
-import basicSsl from '@vitejs/plugin-basic-ssl';
 import vuetify from 'vite-plugin-vuetify';
 
 export default defineConfig({
@@ -19,13 +18,9 @@ export default defineConfig({
 			],
 		},
 	},
-	plugins: [basicSsl(), Vue(), vuetify(), VitePWA(vitePWAconfig)],
+	plugins: [Vue(), vuetify(), VitePWA(vitePWAconfig)],
 	resolve: {
 		alias: [{ find: '@', replacement: fileURLToPath(new URL('./src', import.meta.url)) }],
-	},
-	server: {
-		host: '0.0.0.0',
-		port: 443,
 	},
 	publicDir: 'src/public',
 });
