18615a08864776e34c3c8e038836f1452bd58af8
Timur Enikeev <enikeevt04@gmail.com>
2024-11-07 19:58:32 -0500

    Fix app's session ttl

--- src/utils/time.ts
+++ src/utils/time.ts
@@ -4,3 +4,5 @@ export const formatTime = (ts: number | string) => {
 	const date = new Date(ts);
 	return `${lz(date.getHours())}:${lz(date.getMinutes())}`;
 };
+
+export const msInHour = 60 * 60 * 1000;
