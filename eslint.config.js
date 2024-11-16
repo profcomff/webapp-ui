8eb49033b256fad6fe48d80e21452fcff537e2c3
Artem <55246906+physphile@users.noreply.github.com>
2024-11-16 22:50:13 +0300

    Обновил флоу (#282)
    ## Изменения
    <!-- Опишите здесь на языке, понятном каждому, изменения, сделанные в
    исходном коде по пунктам. -->
    
    ## Детали реализации
    <!-- Здесь можно описать технические детали по пунктам. -->
    
    ## Check-List
    <!-- После сохранения у следующих полей появятся галочки, которые нужно
    проставить мышкой -->
    - [ ] Вы проверили свой код перед отправкой запроса?
    - [ ] Вы написали тесты к реализованным функциям?
    - [ ] Вы не забыли применить форматирование `black` и `isort` для
    _Back-End_ или `Prettier` для _Front-End_?
    
    Co-authored-by: Artem Netsvetaev <physphile@vk.com>

--- eslint.config.js
+++ eslint.config.js
@@ -1,21 +1,9 @@
 import globals from 'globals';
-import path from 'node:path';
-import { fileURLToPath } from 'node:url';
-import js from '@eslint/js';
-import { FlatCompat } from '@eslint/eslintrc';
 import pluginVue from 'eslint-plugin-vue';
 import vueTsEslintConfig from '@vue/eslint-config-typescript';
 import eslint from '@eslint/js';
 import tseslint from 'typescript-eslint';
 import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended';
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-const compat = new FlatCompat({
-	baseDirectory: __dirname,
-	recommendedConfig: js.configs.recommended,
-	allConfig: js.configs.all,
-});
 
 export default [
 	...tseslint.config(eslint.configs.recommended, ...tseslint.configs.recommended),
