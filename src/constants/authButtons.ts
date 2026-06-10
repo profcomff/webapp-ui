6e7db142926ff3e5b8d82c87a83ac4ace9a42cee
Илья Батуев <batuev.io18@physics.msu.ru>
2026-06-11 00:19:51 +0300

    pizdec

--- src/constants/authButtons.ts
+++ src/constants/authButtons.ts
@@ -10,13 +10,6 @@ export const authButtons: AuthButton[] = [
 		color: '#58b4470d',
 	},
 	{
-		name: '@physics.msu.ru',
-		link: 'physics-msu',
-		method: 'physics_auth',
-		icon: `${logos}#ff`,
-		color: '#00014c0d',
-	},
-	{
 		name: '@my.msu.ru',
 		link: 'my-msu',
 		method: 'my_msu_auth',
@@ -36,20 +29,6 @@ export const authButtons: AuthButton[] = [
 		method: 'vk_auth',
 		icon: `${logos}#vk`,
 		color: '#0077ff0d',
-	},
-	{
-		name: 'Github',
-		link: 'github',
-		method: 'github_auth',
-		icon: `${logos}#github`,
-		color: '#24292f0d',
-	},
-	{
-		name: 'Google',
-		link: 'google',
-		method: 'google_auth',
-		icon: `${logos}#google`,
-		color: '#58b4470d',
 	},
 	{
 		name: 'Profcomff ID',
